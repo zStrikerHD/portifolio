ef69defba614382aff4261491c9ddf4e462309a3
Giovani <giovani@teste.com>
2025-09-09 10:27:39 -0300

    Ajustando o modo responssivo, e criando o arquivo para armazenamento das variaveis

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,14 @@
 function createClickEffect(button) {
-            // Remove efeito anterior se existir
             button.classList.remove('click-effect');
             
-            // Força reflow
             button.offsetHeight;
             
-            // Adiciona novo efeito
             button.classList.add('click-effect');
             
-            // Remove após animação
             setTimeout(() => {
                 button.classList.remove('click-effect');
             }, 600);
 
-            // Efeito de vibração no dispositivo (se suportado)
             if (navigator.vibrate) {
                 navigator.vibrate(50);
             }
